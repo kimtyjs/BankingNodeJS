b15e3b662a5997840ce93e2a5b9c7b29494799e8
Taing Kimty <taing.kimty27@gmail.com>
2020-02-09 20:37:25 +0700

    set up dotenv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require("express");
+if( process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
+const express = require("express");
 //Initialize App
 const app = express();
 
